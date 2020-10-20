ba0490224c0a649dc0f3aa8f83280389bd95a1e7
Dylat <56270424+Dylat@users.noreply.github.com>
2020-10-20 13:37:07 -0400

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,10 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@admin.rrz1h.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
---
+++ yourdata.js
@@ -0,0 +1,61 @@
+export default
+    {
+        name :'Dylan Atkins',
+        headerTagline: [
+                        'Dylan Atkins',
+                        
+                        'Web Developer',
+                        'Based out of South Carolina'
+    ],
+        
+        contactEmail:'dylanatkins78@gmail.com',
+        signUp: 'index.html',
+        abouttext: "My name's Dylan and I am 21 years old. I have been coding JavaScript, CSS, and HTML for a little over a year. I enjoy front-end web developing as it suits me better than back-end. ",
+        aboutImage:'https://i.imgur.com/vOzVxrB.jpg',
+       
+       ShowAboutImage:false,
+        
+       projects:[
+           {
+            id: 1,
+            title:'Song Search API', 
+             service:'API', 
+             imageSrc:"https://i.imgur.com/BvGTnyS.png"
+             
+            },
+            {
+                id: 2,
+                title: 'Food Blog',
+                service: 'Website Development',
+                imageSrc: "https://i.imgur.com/htmp1nG.png"
+                
+            },/*
+            { 
+                id: 3,
+                title: 'Food Blog Front End ',
+                service: 'Web App',
+                imageSrc: "https://i.imgur.com/htmp1nG.png"
+                
+            },
+            {
+                id: 4,
+                title: 'Project Four',
+                service: '',
+                imageSrc: ""
+                
+           } */
+
+               
+        ],
+        social: [
+            
+            {   name:'Github',
+                url:'https://github.com/Dylat'},
+           
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/dylat_/'
+            }
+
+        ]
+    }
